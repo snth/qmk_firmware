96573f7d5cbd1003be3c35611b5d344ec5d07df0
Tobias Brandt <Tobias.Brandt@gmail.com>
2021-01-12 12:28:57 +0200

    Changes the crkbd keymap layout to resemble the physical crkbd layout more

--- keyboards/crkbd/keymaps/snth/keymap.c
+++ keyboards/crkbd/keymaps/snth/keymap.c
@@ -9,16 +9,16 @@
  */
 
 #define LAYOUT_crkbd_base( \
-    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, \
-    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
-    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, \
+    K01, K02, K03, K04, K05,           K06, K07, K08, K09, K0A, \
+    K11, K12, K13, K14, K15,           K16, K17, K18, K19, K1A, \
+    K21, K22, K23, K24, K25,           K26, K27, K28, K29, K2A, \
                    K34, K35, K36, K37, K38, K39       \
   ) \
   LAYOUT_wrapper( \
-    KC_TAB,  K01,   K02,   K03,    K04,     K05,                              K06,     K07,     K08,     K09,     K0A,    KC_BSPC,   \
-    CK_COLN, K11,   K12,   K13,    K14,     K15,                              K16,     K17,     K18,     K19,     K1A,    KC_MINS,   \
-    CTL_ESC, K21,   K22,   K23,    K24,     K25,                              K26,     K27,     K28,     K29,     K2A,    CTL_BSLS,  \
-                   		   K34,     K35,      K36,           K37,     K38,     K39 					     \
+    KC_TAB,   K01,  K02,  K03,  K04,  K05,                       K06,  K07,  K08,  K09,  K0A,    KC_BSPC,   \
+   CK_COLN,   K11,  K12,  K13,  K14,  K15,                       K16,  K17,  K18,  K19,  K1A,    KC_MINS,   \
+   CTL_ESC,   K21,  K22,  K23,  K24,  K25,                       K26,  K27,  K28,  K29,  K2A,    CTL_BSLS,  \
+                                K34,  K35,  K36,           K37,  K38,  K39 					     \
     )
 #define LAYOUT_crkbd_base_wrapper(...)       LAYOUT_crkbd_base(__VA_ARGS__)
 
